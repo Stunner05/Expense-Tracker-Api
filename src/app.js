d188ae9c169173905d4d087779eb05d1766b9a92
Presh <akintobiprecious@gmail.com>
2025-09-24 13:50:42 +0100

    Setup vercel deployment for Express API

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const config = require('../config')
 const multer = require("multer");
 const upload = multer();
 const routes = require('./routes/index')
-
+const api = require('../api')
 const app = express()
 
 app.get("/favicon.ico", (req, res) => res.status(204).end());
@@ -18,13 +18,17 @@ app.get("/ping", (req, res) => {
 app.get("/api/test", (req, res) => res.json({ ok: true }));
 
 
-
 app.use(cors({
     // origin: config.frontrnd_uri,
     methods: ['GET', 'PATCH','PUT', 'POST', 'DELETE']
 }))
+
+
 app.use(helmet())
 app.use(express.json({limit: '2000kb'}))
+
+api.use("/api/v1", app);
+
 // app.use(xss)
 app.use((req, res, next) =>{
     console.log(req.path, req.method)
